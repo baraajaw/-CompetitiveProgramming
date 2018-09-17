a266d0ca37e26f3b5702dc3ae708d165deaceff3
Bara' Al-Jawarneh <baraaaljawarneh@gmail.com>
2018-09-17 11:35:55 +0300

    Update PT07Z - Longest path in a tree.cpp

--- SPOJ/PT07Z - Longest path in a tree.cpp
+++ SPOJ/PT07Z - Longest path in a tree.cpp
@@ -1,36 +1,35 @@
 #include <bits/stdc++.h>
-#define f(i, x, n) for (int i = x; i < (int)(n); ++i)
-#define ll long long
 using namespace std;
-int n, ans = 0, at ;
-bool vis[10000];
+#define ll long long 
+int n, mxLen, at;
 vector < vector < int > > g;
-void DFS(int node, int cnt){
-    vis[node] = true;
-    f(i, 0, g[node].size())
-        if (!vis[g[node][i]])
-              DFS(g[node][i], cnt + 1);
-    if ( cnt > ans)        
-        ans = cnt , at = node;         
 
+void DFS(int v, int par, int len){
+  for (int i = 0; i < g[v].size(); ++i)
+    if (g[v][i] != par)
+      DFS(g[v][i], v, len + 1);
+    if(len > mxLen){
+      mxLen = len;
+      at = v;
+    }
 }
-int main() {
+int  main() {
   while (scanf("%d", &n) > 0){
-        ans = 0;  
-        g.clear();  
-        g.resize(n);
-        memset(vis, false, sizeof vis);
-        f(i, 0, n - 1){
-            int a, b;
-            scanf("%d%d", &a, &b);
-            --a; --b;
-            g[a].push_back(b);
-            g[b].push_back(a);
-        }
-        DFS(0,0);
-        memset(vis, false, sizeof vis);
-        DFS(at, 0);
-        printf("%d\n", ans );
+    g.clear();
+    g.resize(n);
+    int u, v;
+    for ( int i = 1; i < n; ++i){
+      scanf("%d%d", &u, &v);
+      --u; --v;
+      g[u].push_back(v);
+      g[v].push_back(u);
+    }
+    mxLen = -1;
+    DFS(0, -1, 0);
+    DFS(at, -1, 0);
+    printf("%d\n", mxLen);
   }
+  
   return 0;
 }
+ 
