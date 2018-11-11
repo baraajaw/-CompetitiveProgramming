eca132a0c90f022464913be64249c3763fb6c96c
Bara' Al-Jawarneh <baraaaljawarneh@gmail.com>
2018-11-11 19:23:50 +0200

    DFS

--- UVA/280 - Vertex.cpp
+++ UVA/280 - Vertex.cpp
@@ -1,50 +1,49 @@
 #include <bits/stdc++.h>
-#define ll long long 
-#define f(i, x, n) for(int i = x; i < (int)(n); ++i)
 using namespace std;
-int n, q, x, y;
+int v, a, b, c;
+vector<vector<int> > g;
+vector<int> res;
 bool vis[101];
-vector < vector < int > > g;
-vector < int > ans;
-void dfs(int node){
-  f(i, 0, g[node].size()){
-    if (!vis[g[node][i]]){
-      vis[g[node][i]] = 1;
-      dfs(g[node][i]);
-    }
-  }
+
+void DFS(int src){
+	for(int i = 0; i < g[src].size(); i++)
+		if(!vis[g[src][i]]) {
+	        vis[g[src][i]] = true;
+	        DFS(g[src][i]);
+	    }
+
 }
 int main() {
-  
-  while ( scanf("%d", &n) && n != 0){
-    g.clear();
-    g.resize(n);
-    while( scanf("%d", &x) && x != 0) {
-        --x;
-        while ( scanf("%d", &y) && y != 0){
-          --y;
-          g[x].push_back(y);
-        }
-    }
-    scanf("%d", &q);
-    while ( q--){
-      scanf ("%d", &x);
-      memset(vis, 0, sizeof vis);
-      --x;
-      dfs(x);
-      f(i, 0, n){
-        if( !vis[i])
-          ans.push_back(i + 1);
-      }
-      printf("%d", ans.size());
-      f(i, 0, ans.size())
-          printf(" %d", ans[i] );
-      puts("");
-      ans.clear();    
-    }
-  }
-  
+	while(scanf("%d", &v) == 1 && v){
 
+		g.clear();
+		g.resize(v);
 
-  return 0;
+		while(scanf("%d", &a) && a)
+			while(scanf("%d", &b) && b)
+				g[a - 1].push_back(b - 1);
+			
+		scanf("%d", &a);
+		while(a--){
+			res.clear();
+			scanf("%d", &b);
+			memset(vis, false, sizeof vis);
+			DFS(--b);
+
+			c = 0;
+			for(int i = 0; i < v; ++i)
+				if(!vis[i]) {
+                    c++;
+                    res.push_back(i+1);
+                }
+            printf("%d", c);
+            for(int i = 0; i < res.size(); i++)
+                printf(" %d", res[i]);
+            putchar('\n');
+		}
+
+	}
+
+    return 0;
+
 }
