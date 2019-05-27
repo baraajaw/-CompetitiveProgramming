288eea5006ec4a62f04e6f324aab3bc43dbd3151
Bara' Al-Jawarneh <baraaaljawarneh@gmail.com>
2019-05-27 15:05:48 +0300

    Implementation

--- UVA/10038 - Jolly Jumpers.cpp
+++ UVA/10038 - Jolly Jumpers.cpp
@@ -1,27 +1,28 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define ll long long 
 
-int  main() {
-  int n, a[3003];
-  bool f = true;
-  vector < int > v;
-  while (scanf("%d", &n) > 0){
-    v.clear();
-    f = true;
-    for (int i = 0; i < n; ++i)
-      scanf("%d", &a[i]);
-    for (int i = 1; i < n; ++i)
-      v.push_back(abs(a[i - 1] - a[i])); 
-    sort(v.begin(), v.end());
-    for(int i = 0; i < v.size(); ++i)
-        if(v[i] != i + 1)
-          f = false;
-    if(f)
-      puts("Jolly");
-    else
-      puts("Not jolly");      
-  }
-  return 0;
+set<int> st;
+
+int main(){
+    
+
+     int n,  a[3003];
+
+     while (scanf("%d", &n) > 0){
+        st.clear();
+        for (int i = 0; i < n; ++i)
+            scanf("%d", &a[i]);
+        
+        for (int i = 1; i < n; ++i)
+            if (abs(a[i] - a[i - 1]) >= 1 && abs(a[i] - a[i - 1]) < n)
+                st.insert(abs(a[i] - a[i - 1]));
+        if (st.size() == n - 1)
+            puts("Jolly");
+        else
+            puts("Not jolly");
+
+     }
+
+
+
 }
- 
